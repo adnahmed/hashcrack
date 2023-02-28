5166ec9fad26b98657d6ef4a7d1b2f2b19d920f8
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-02-28 19:15:51 +0500

    Add ErrorBoundary component for improved error handling

---
+++ components/ErrorBoundary.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    // Define a state variable to track whether there is an error or not
+    this.state = { hasError: false };
+  }
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI
+
+    return { hasError: true };
+  }
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+    // Use Error Logging Serive e.g. Sentry here
+    console.log({ error, errorInfo });
+  }
+  render(): React.ReactNode {
+    // Check if error is thrown
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h2>Oops, there is an error!</h2>
+          <button
+            type="button"
+            onClick={() => this.setState({ hasError: false })}
+          >
+            Try Again?
+          </button>
+        </div>
+      );
+    }
+    // Return children components in case of no error
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
