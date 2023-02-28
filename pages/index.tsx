7bab0a824a763650c298caa9149872c5ee2422cf
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-02-28 21:14:58 +0500

    Add Layout, Stats, TaskQueue and global style updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
+import Layout from "@/components/Layout";
+import Stats from "@/components/Stats";
 import styles from "@/styles/Home.module.css";
 import { Inter } from "@next/font/google";
 import Head from "next/head";
-import { Suspense } from "react";
-
+import { NextPageWithLayout } from "./_app";
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+const Home: NextPageWithLayout = () => {
   return (
     <>
       <Head>
@@ -15,23 +16,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.title}>
-          <h1 className={styles.bottomBorder}>HashCrack</h1>
-        </div>
-        <li className={styles.navbar}>
-          <ul>Tasks queue</ul>
-          <ul>Add new task</ul>
-          <ul>Get Result</ul>
-          <ul>Verify Service</ul>
-          <ul>Contact Us</ul>
-        </li>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Stats />
-        </Suspense>
+        <Stats />
+        <TaskQueue />
       </main>
     </>
   );
-}
+};
 
 export default Home;
 
@@ -39,25 +29,6 @@ Home.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>;
 };
 
-function Stats() {
-  return (
-    <div className={styles.stats}>
-      <p>Tasks queued: user </p>
-      <p>WPA processed:</p>
-      <p>WPA cracked: %</p>
-      <p>Hashes processed: _. </p>
-      <p>Hashes cracked: %</p>
-      <p>GPU cluster spped: user</p>
-    </div>
-  );
-}
-function useGPUState() {
-  return GPUState.IDLE;
-}
-function GPUStatus() {
-  return (
-    <div className={styles.tooltip}>
-      <p>status </p>
-    </div>
-  );
+function TaskQueue() {
+  return <div>TAAASSSKKKS</div>;
 }
