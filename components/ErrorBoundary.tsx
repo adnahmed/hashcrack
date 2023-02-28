29bdc777dcbd6b3049d859565b4d82b8e4c8e1a1
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-02-28 22:18:24 +0500

    Update ErrorBoundary to add type safety to props and state

--- components/ErrorBoundary.tsx
+++ components/ErrorBoundary.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-class ErrorBoundary extends React.Component {
-  constructor(props) {
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
     super(props);
     // Define a state variable to track whether there is an error or not
     this.state = { hasError: false };
   }
-  static getDerivedStateFromError(error) {
+  static getDerivedStateFromError(error: Error) {
     // Update state so the next render will show the fallback UI
 
     return { hasError: true };
