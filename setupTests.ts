162bc45a1252cdadbfbb889690402ccf6c60d6a4
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-03-06 10:31:41 +0500

    Update setupTests and tsconfig.test to include react-jsx extension

--- setupTests.ts
+++ setupTests.ts
@@ -1,3 +1,3 @@
 import { loadEnvConfig } from '@next/env'
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom/extend-expect'
 loadEnvConfig(__dirname, true, { info: () => null, error: console.error })
