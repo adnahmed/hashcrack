ce4300c3775d82e0b0e0c9fda86f721c14e9e694
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-03-06 11:00:45 +0500

    Add Redux store to App component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
+import store from "@/lib/redux/store";
 import "@/styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 import "@total-typescript/ts-reset";
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
 import { ReactElement, ReactNode } from "react";
+import { Provider } from "react-redux";
 import ErrorBoundary from "../components/ErrorBoundary";
-
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
@@ -18,8 +19,10 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
   return (
-    <NextUIProvider>
-      <ErrorBoundary>{getLayout(<Component {...pageProps} />)}</ErrorBoundary>
-    </NextUIProvider>
+    <Provider store={store}>
+      <NextUIProvider>
+        <ErrorBoundary>{getLayout(<Component {...pageProps} />)}</ErrorBoundary>
+      </NextUIProvider>
+    </Provider>
   );
 }
