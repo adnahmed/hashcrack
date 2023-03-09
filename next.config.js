1270607e132b6ede4de8d882652986f7bfd28160
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-03-09 14:37:15 +0500

    Add environment variables to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    DEFAULT_DB_URL: "postgresql://postgres:@localhost:5432/hashcrack",
+    CAPTCHA_LETTER_NUM: 4,
+    CAPTCHA_VERIFY_MAXAGE: 60 * 6 * 24,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
