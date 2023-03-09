7513aaf3107bf98fbed1ab1d86b7f803de8b2d61
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-03-09 15:10:34 +0500

    Add Keyv caching with Postgres store and usage example

---
+++ lib/cache.ts
@@ -0,0 +1,31 @@
+import Keyv from "@keyvhq/core";
+import KeyvPostgres from "@keyvhq/postgres";
+export function keyvStoreFactory() {
+    const cache = new Keyv({
+        store: new KeyvPostgres(process.env.DATABASE_URL || process.env.DEFAULT_DB_URL)
+    }) // in-memory, by default
+
+    // Handle database connection errors
+    cache.on('error', err => console.log('Connection Error', err)) // TODO: Improve Error Handling
+    return cache
+}
+
+const cache = keyvStoreFactory()
+export const geo = keyvStoreFactory()
+export const captcha = keyvStoreFactory()
+export default cache;
+
+/*
+USAGE: 
+_----_
+
+- BASICS
+await cache.set('foo', 'expires in 1 second', 1000) // true
+await cache.set('foo', 'never expires') // true
+await cache.get('foo') // 'never expires'
+await cache.has('foo') // true
+await cache.delete('foo') // true
+await cache.has('foo') // false
+await cache.clear() // undefined
+
+*/
