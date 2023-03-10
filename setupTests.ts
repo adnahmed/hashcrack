aa4c42b7e23b9fc54f956f5cd64a88486d73b930
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-03-10 13:11:54 +0500

    Add JSDOM mocks and isomorphic-fetch to setupTests.ts

--- setupTests.ts
+++ setupTests.ts
@@ -1,3 +1,18 @@
-import { loadEnvConfig } from '@next/env'
-import '@testing-library/jest-dom/extend-expect'
+import { loadEnvConfig } from '@next/env';
+import '@testing-library/jest-dom/extend-expect';
+import 'isomorphic-fetch';
 loadEnvConfig(__dirname, true, { info: () => null, error: console.error })
+/* JSDOM Mocks */
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    })),
+});
