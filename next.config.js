d1a04c4d0aac6629b481fc4ca3cd88517350dbd8
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-03-12 11:27:33 +0500

    Update environment variables to add dCAPTCHA_DB_URL and dGEO_DB_URL

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    DEFAULT_DB_URL: "postgresql://postgres:@localhost:5432/hashcrack",
+    dCAPTCHA_DB_URL: "postgresql://postgres:@localhost:5432/hashcrack_captcha",
+    dGEO_DB_URL: "postgresql://postgres:@localhost:5432/hashcrack_geo",
     CAPTCHA_LETTER_NUM: 4,
     CAPTCHA_VERIFY_MAXAGE: 60 * 6 * 24,
   },
