f11aaa7b662abc986804c0d4774f4f5b911125f8
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-03-12 11:26:45 +0500

    Update baseQuery to add prepareHeaders function and AppState type

--- features/api/apiSlice.ts
+++ features/api/apiSlice.ts
@@ -1,4 +1,5 @@
 // Import the RTK Query methods from the React-specific entry point
+import { AppState } from '@/lib/redux/store'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { HYDRATE } from 'next-redux-wrapper'
 
@@ -17,7 +18,19 @@ export const apiSlice = createApi({
         if (action.type === HYDRATE) return action.payload[reducerPath]
     },
     tagTypes: [],
-    baseQuery: fetchBaseQuery(), // TODO: check and verify proper envs
+    // TODO: check and verify proper envs
+    baseQuery: fetchBaseQuery({
+        baseUrl: '/',
+        prepareHeaders: (headers, { getState }) => {
+            // By default, if we have a token in the store, let's use that for authenticated requests
+            const state = (getState() as AppState)
+            state.api
+            // if (token) {
+            // headers.set('captcha_token', token)
+            // }
+            return headers
+        },
+    }),
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     endpoints: builder => ({
         /* Global Endpoints */
