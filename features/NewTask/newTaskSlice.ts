1cd2a7c5ed5ab24a04e50b85a4fdb67366cb793b
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-03-12 11:31:08 +0500

    Remove unnecessary getCaptcha and verifyCaptcha mutations from newTaskSlice.ts

--- features/NewTask/newTaskSlice.ts
+++ features/NewTask/newTaskSlice.ts
@@ -4,22 +4,9 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
     endpoints: builder => ({
         addTask: builder.mutation({
             query: (task) => '/task/new'
-        }),
-        getCaptcha: builder.query<{ captcha: string }, null>({
-            query: () => ({
-                url: '/api/captcha',
-                method: 'GET'
-            })
-        }),
-        verifyCaptcha: builder.mutation<{ result: boolean }, string>({
-            query: (answer) => ({
-                url: `/api/captcha/verify`,
-                method: 'POST',
-                body: { answer }
-            })
         })
     })
 })
 
 
-export const { useAddTaskMutation, useGetCaptchaQuery, useVerifyCaptchaMutation } = extendedApiSlice
+export const { useAddTaskMutation } = extendedApiSlice
