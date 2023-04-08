22921088f86ac3613b2d6f30c5a3b5c93c728b67
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-04-08 06:27:19 +0500

    fix: wdyr error on tracking useSelector

--- scripts/wdyr.ts
+++ scripts/wdyr.ts
@@ -2,15 +2,11 @@
 /// <reference types="@welldone-software/why-did-you-render" />
 import whyDidYouRender from '@welldone-software/why-did-you-render'
 import React from 'react'
-import * as ReactRedux from 'react-redux'
 
 if (process.env.NODE_ENV === 'development') {
   if (typeof window !== 'undefined') {
     whyDidYouRender(React, {
       trackAllPureComponents: true,
-      trackExtraHooks: [
-        [ReactRedux, 'useSelector']
-      ]
     })
   }
 }
