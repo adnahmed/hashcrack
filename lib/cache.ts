432dcfb985bfd3b2e11a0fd59f6a12d43bdf7763
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-04-18 13:26:19 +0500

    add redis-save to crackq

--- lib/cache.ts
+++ lib/cache.ts
@@ -1,29 +1,7 @@
-import Keyv from "@keyvhq/core";
-import KeyvPostgres from "@keyvhq/postgres";
-const db_url = {
-    captcha: process.env.CAPTCHA_DB_URL || process.env.dCAPTCHA_DB_URL,
-    geo: process.env.GEO_DB_URL || process.env.dGEO_DB_URL
-}
-export function keyvStoreFactory(url: string) {
-    return new Keyv({
-        store: new KeyvPostgres(url)
-    }).on('error', err => console.warn(err)) // TODO: Improve Error Handling 
-}
+import { createClient } from "redis";
 
-export const geoKv = keyvStoreFactory(db_url.geo)
-export const captchaKv = keyvStoreFactory(db_url.captcha)
+const redis = createClient({ url: process.env.REDIS_URL });
 
-/*
-USAGE: 
-_----_
+redis.on("error", (err) => console.log("Redis Client Error", err));
 
-- BASICS
-await cache.set('foo', 'expires in 1 second', 1000) // true
-await cache.set('foo', 'never expires') // true
-await cache.get('foo') // 'never expires'
-await cache.has('foo') // true
-await cache.delete('foo') // true
-await cache.has('foo') // false
-await cache.clear() // undefined
-
-*/
+await redis.connect();
