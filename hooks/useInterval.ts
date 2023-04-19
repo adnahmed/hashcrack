24bb2af679fda59206478f3e38e8110120971199
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-04-19 21:20:23 +0500

    refactor useInterval

--- hooks/useInterval.ts
+++ hooks/useInterval.ts
@@ -1,16 +1,18 @@
 import { useEffect, useRef } from "react";
 
+// eslint-disable-next-line @typescript-eslint/ban-types
 export const useInterval = (callback: Function, delay: number) => {
-    const savedCallback = useRef<Function>()
-    useEffect(() => {
-        savedCallback.current = callback
-    }, [callback])
-    useEffect(() => {
-        const handler = (...args: any) => savedCallback.current?.(...args)
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const savedCallback = useRef<Function>();
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+  useEffect(() => {
+    const handler = (...args: any) => savedCallback.current?.(...args);
 
-        if (delay !== null) {
-            const id = setInterval(handler, delay)
-            return () => clearInterval(id)
-        }
-    }, [delay])
-}
+    if (delay !== null) {
+      const id = setInterval(handler, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+};
