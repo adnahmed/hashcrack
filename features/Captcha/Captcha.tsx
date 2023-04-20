7102db83169267ec148e076dbd467379861a1334
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-04-20 12:12:01 +0500

    add captcha component

--- features/Captcha/Captcha.tsx
+++ features/Captcha/Captcha.tsx
@@ -1,49 +1,70 @@
-// TODO: Replace with ReCaptcha Cloudflare.
-import Image from "next/image";
-import { ChangeEventHandler, useState } from "react";
+import { useAppDispatch } from "@/hooks/useApp";
+import type {
+  TurnstileInstance,
+  TurnstileProps,
+} from "@marsidev/react-turnstile";
+import { Turnstile } from "@marsidev/react-turnstile";
+import { useTheme } from "next-themes";
+import { useEffect, useRef, useState } from "react";
+import {
+  captchaFailed,
+  tokenValidated,
+  useValidateTokenMutation,
+} from "./captchaSlice";
+import { Lang, Theme, WidgetSize, WidgetStatus } from "./types";
 
-import { useGetCaptchaQuery, useVerifyCaptchaMutation } from "./captchaSlice";
-import { useAnswer } from "./lib";
+interface Props extends Omit<TurnstileProps, "siteKey"> {
+  initialTheme?: Theme;
+  initialSize?: WidgetSize;
+  initialLang?: Lang;
+}
 
-export default function Captcha() {
-  const [answer, setAnswer] = useState("");
-  const { data, isLoading, isFetching, isSuccess, isError, error, refetch } =
-    useGetCaptchaQuery(null, { refetchOnReconnect: true });
-  const setAnswerValue: ChangeEventHandler<HTMLInputElement> = (e) =>
-    setAnswer(e.target.value);
-  const [verifyCaptcha, { isLoading: isVerifying }] =
-    useVerifyCaptchaMutation();
-  useAnswer({ answer, setAnswer, refetch, verifyCaptcha });
+export default function Captcha({
+  initialTheme,
+  initialSize,
+  initialLang,
+  ...props
+}: Props) {
+  const siteTheme = useTheme().resolvedTheme as Theme | undefined;
+  const [theme, setTheme] = useState<Theme | undefined>(
+    initialTheme ?? siteTheme
+  );
+  const [size, setSize] = useState<WidgetSize>(initialSize ?? "normal");
+  const [lang, setLang] = useState<Lang>(initialLang ?? "auto");
+  const [status, setStatus] = useState<WidgetStatus>(null);
+  const [token, setToken] = useState<string>();
+
+  useEffect(() => {
+    if (siteTheme && !theme) {
+      setTheme(siteTheme as Theme);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [siteTheme]);
+
+  const turnstileRef = useRef<TurnstileInstance>(null);
+  const [validateToken, { isError, isSuccess, error, isLoading }] =
+    useValidateTokenMutation();
+  const dispatch = useAppDispatch();
   return (
     <>
-      {isLoading || isFetching ? (
-        <div>...</div>
-      ) : isError ? (
-        <div>
-          error loading captcha {JSON.stringify(error)}{" "}
-          {/* TODO: improve error handling*/}
-        </div>
-      ) : (
-        isSuccess &&
-        data && (
-          <>
-            <Image
-              unoptimized
-              width="200"
-              height="72"
-              alt="captcha"
-              src={`data:image/png;base64,${data.captcha}`}
-            />
-            <input
-              value={answer}
-              disabled={isVerifying}
-              type="text"
-              onChange={setAnswerValue}
-            />
-          </>
-        )
-      )}
-      <button onClick={refetch}>retry</button>
+      <Turnstile
+        {...props}
+        ref={turnstileRef}
+        siteKey={
+          process.env.NODE_ENV === "development"
+            ? // Always pass on dev
+              "1x00000000000000000000AA"
+            : process.env.CFSITE_KEY
+        }
+        onSuccess={async (token) => {
+          const res = await validateToken(token).unwrap();
+          if (res.success) dispatch(tokenValidated(token));
+          // Error encountered validating Captcha
+          if (res["error-codes"]) {
+            dispatch(captchaFailed(res["error-codes"]));
+          }
+        }}
+      />
     </>
   );
 }
