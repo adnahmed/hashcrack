eee5eccb220e14f8e0021a307401b477aceffe13
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-04-24 03:03:10 +0500

    initial check storybook

---
+++ stories/components/InitialCheck.stories.tsx
@@ -0,0 +1,12 @@
+import InitialCheck from "@/components/ui/IntialCheck";
+
+export default {
+  title: "Components/InitialCheck",
+  component: InitialCheck,
+};
+
+export const InitialCheckPage = () => (
+  <InitialCheck>
+    <></>
+  </InitialCheck>
+);
