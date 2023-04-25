3ac23121046c361775d009b776baf650276085fb
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-04-25 09:22:43 +0500

    fix title

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,25 +1,7 @@
-import styles from "@/styles/Title.module.css";
-import clsx from "clsx";
-import { useState } from "react";
 export default function Title() {
-  const [hovered, setHover] = useState(false);
-
-
   return (
-    <div
-      className={styles.title}
-      onPointerEnter={() => setHover(true)}
-      onPointerLeave={() => setHover(false)}
-    >
-      <h1
-        className={clsx({
-          bottomBorderMd: hovered,
-          paddingBottomMd: true,
-          animationXs: true,
-        })}
-      >
-        crackq.me
-      </h1>
+    <div>
+      <h1>crackq.me</h1>
     </div>
   );
 }
