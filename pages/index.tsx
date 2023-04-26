d8b02c6f30a71839c7bc887b7bbcc2714e0400f5
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-04-26 18:42:59 +0500

    fix: only one main

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,12 +17,12 @@ const Home: NextPageWithLayout = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <div className={styles.main}>
         {/* TODO: Use Spinner */}
         <Suspense fallback={<div>Loading...</div>}>
           {!captchaValidated ? <IntialCheck /> : <MainPage />}
         </Suspense>
-      </main>
+      </div>
     </>
   );
 };
