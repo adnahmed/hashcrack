ff136c26d3d5c2432722139f6d0b9b39fae03932
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-04-26 18:43:25 +0500

    add css for InitialCheck

--- components/ui/InitialCheck.tsx
+++ components/ui/InitialCheck.tsx
@@ -1,18 +1,25 @@
 import Captcha from "@/features/Captcha/Captcha";
-
+import style from "@/styles/InitialCheck.module.css";
+import Image from "next/image";
 function IntialCheck() {
   return (
-    <div>
-      <div>
-        <span>Please wait while we are checking your browser...</span>
-      </div>
-      <div>
-        <span>Connection Problems? Let us Know</span>
-        <div>
-          <a>Contact Us</a> <a> Status</a>
-        </div>
-      </div>
-      <Captcha />
+    <div className={style.main}>
+      <Image
+        alt="Logo"
+        width={300}
+        height={300}
+        src="/favicon.ico"
+        className={style.logo}
+      />
+      <span className={style.waitmessage}>
+        Please wait while we are checking your browser...
+      </span>
+      <Captcha className={style.captcha} />
+      <span className={style.connectionquery}>
+        Connection Problems? Let us Know
+      </span>
+      <a className={style.contactus}>Contact Us</a>{" "}
+      <a className={style.status}> Status</a>
     </div>
   );
 }
