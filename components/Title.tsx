b5f1167c899c1ee09c9eb1af00ae472da9b30cc4
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-04-29 01:48:39 +0500

    add Title Component

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,4 +1,4 @@
-import styles from "@/styles/Title.module.css";
+import styles from "@/styles/Title.module.scss";
 import { AnimeAnimParams } from "animejs";
 import { HTMLProps } from "react";
 import ReactAnime from "./Anime";
@@ -9,15 +9,14 @@ interface TitleProps {
 
 export default function Title(props: TitleProps & HTMLProps<"div">) {
   const { text } = props;
-  /* TODO: Use Splitting: https://splitting.js.org */
-  const content = text.split("");
-  let letter = 0;
+  /* TODO: Use Splitting: https://splitting.js.org and Anime.js for Effect */
+  const content = text;
   const animation: AnimeAnimParams = {
-    targets: `#letter`,
-    translateY: ["0.4em", 0],
+    targets: `#title`,
+    translateY: ["0.75em", 0],
     translateZ: 0,
     duration: 750,
-    delay: (el, i) => 10 * i,
+    // delay: (el, i) => 10 * i,
   };
   return (
     <ReactAnime.Anime
@@ -27,14 +26,8 @@ export default function Title(props: TitleProps & HTMLProps<"div">) {
       _onClick={[animation]}
       initial={[animation]}
     >
-      <div id="ml6" className={styles.ml6}>
-        <div className={styles.textWrapper}>
-          {content.map((c) => (
-            <span id="letter" className={styles.letter} key={letter++}>
-              {c}
-            </span>
-          ))}
-        </div>
+      <div id="title" className={`${styles.h1}`}>
+        {content}
       </div>
     </ReactAnime.Anime>
   );
