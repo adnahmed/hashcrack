f211342ff73d638d3d1e46be317e5b93478f6688
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-04-30 15:41:55 +0500

    adjust MainPage and add styles

--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -1,10 +1,14 @@
+import Logo from "@/public/favicon.svg";
+import style from "@/styles/MainPage.module.css";
 import Navbar from "./Navbar";
 import Title from "./Title";
-
 const MainPage = () => {
   return (
-    <div>
-      <Title text="Crackq.Me" />
+    <div className={style.main}>
+      <div className={style.heading}>
+        <img src={Logo} alt="Logo" className={style.logo} />
+        <Title text="Crackq.Me" />
+      </div>
       <Navbar />
     </div>
   );
