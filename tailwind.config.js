34a14f761183c151f42b2f68a11c44b39396c6b7
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-05-20 11:50:56 +0500

    remove utopia config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,32 +10,7 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
-  extend: {
-    utopia: theme => ({
-      minScreen: theme('screens.sm'),
-      maxScreen: theme('screens.xl'),
-      maxScale: 1.5,
-      minSize: 16,
-      maxSize: 20,
-      minScale: 1.2,
-      textSizes: [
-        'xs',
-        'sm',
-        'base',
-        'lg',
-        'xl',
-        '2xl',
-        '3xl',
-        '4xl',
-        '5xl',
-        '6xl',
-        '7xl',
-        '8xl',
-        '9xl',
-      ],
-    }),
-    fluidTypography: {},
-  },
+  extend: {},
   plugins: [
     require('tailwind-utopia')({
       useClamp: true,
