4f6b99a965ba85d88283522bfbff0cc3ddf17372
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-05-21 13:13:30 +0500

    remove extra div from task queue

--- components/TaskQueue.tsx
+++ components/TaskQueue.tsx
@@ -1,10 +1,12 @@
 import Stats from "@/components/Stats";
 import TaskTable from "./TaskTable";
+
 export default function TasksQueue() {
+
   return (
-    <div>
+    <>
       <Stats />
       <TaskTable />
-    </div>
+    </>
   );
 }
