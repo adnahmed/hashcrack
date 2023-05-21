c2f4ad8390c2c277e1f56bb3cec184156d3240ae
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-05-21 13:14:11 +0500

    make tabs composable

--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -5,19 +5,46 @@ import style from "@/styles/MainPage.module.css";
 import { useTheme } from "next-themes";
 import Navbar from "./Navbar";
 import Title from "./Title";
+import { useBreakpoint } from "@/hooks/useBreakpoint";
+import { ReactNode, useEffect, useRef, useState } from "react";
+import { TabsRef } from "flowbite-react";
+import NewTask from "@/features/NewTask/NewTask";
+import ContactUs from "./ContactUs";
+import TaskQueue from "./TaskQueue";
+interface Tabs {
+  [key: string]: ReactNode
+}
 const MainPage = () => {
+  const TabsEnum: Tabs = {
+    "Tasks Queue": <TaskQueue />,
+    "Add New Tasks": <NewTask />,
+    "Get Result": <div></div>,
+    "Verify Service": <div></div>,
+    "Contact Us": <ContactUs />
+  }
+  const TabLabels = Object.keys(TabsEnum)
   const { theme, setTheme } = useTheme();
+  const [openNavbar, setOpenNavbar] = useState(false);
+  const [activeTab, setActiveTab] = useState<number>(0);
+  const tabsRef = useRef<TabsRef>(null);
+  const isDesktop = useBreakpoint('lg2');
+  useEffect(() => {
+    if (isDesktop) setOpenNavbar(true)
+    else setOpenNavbar(false)
+  }, [isDesktop])
+
   return (
-    <div className={`py-fl-md`}>
+    <>
       <div
         className={`
       flex
       max-h-fl-2xl-3xl
       items-center 
       justify-between
-      px-fl-lg
-      md:justify-around
-      xl:justify-evenly
+      md:px-0
+      sm:justify-around
+      xl:justify-center
+      xl:gap-fl-xl
       `}
       >
         {
@@ -31,7 +58,7 @@ const MainPage = () => {
             <img
               src={Sun}
               alt={"light-theme"}
-              className={`h-fl-lg w-fl-lg `}
+              className={`m-fl-3xs h-fl-xs w-fl-xs`}
               onClick={() => setTheme("light")}
             />
           ) : (
@@ -44,9 +71,18 @@ const MainPage = () => {
             />
           )}
         </div>
+        {!isDesktop && <button onClick={() => { setOpenNavbar(!openNavbar) }} className={`hamburger hamburger--collapse ${openNavbar && 'is-active'}`} type="button">
+          <span className="hamburger-box">
+            <span className="hamburger-inner">
+            </span>
+          </span>
+        </button>}
       </div>
-      <Navbar />
-    </div>
+      {openNavbar &&
+        <Navbar ref={tabsRef} TabLabels={TabLabels} activeTab={activeTab} setActiveTab={setActiveTab} />
+      }
+      {TabsEnum[TabLabels[activeTab]]}
+    </>
   );
 };
 
