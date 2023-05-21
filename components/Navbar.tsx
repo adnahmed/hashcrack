bf94c1f2cc088fb726e35b947625d126c5489500
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-05-21 13:13:55 +0500

    make navbar composable

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,39 @@
-import NewTask from "@/features/NewTask/NewTask";
-import { Tabs } from "flowbite-react";
-import ContactUs from "./ContactUs";
-import TaskQueue from "./TaskQueue";
-export default function Navbar() {
-  return (
-    <div>
-      <Tabs.Group aria-label="Full width tabs" style="default">
-        <Tabs.Item title="Tasks Queue">
-          <TaskQueue />
-        </Tabs.Item>
-        <Tabs.Item title="Add New Task">
-          <NewTask />
-        </Tabs.Item>
-        <Tabs.Item title="Get Result">Get Result</Tabs.Item>
-        <Tabs.Item title="Verify Service">Verify Service</Tabs.Item>
-        <Tabs.Item title="Contact Us">
-          <ContactUs />
-        </Tabs.Item>
-      </Tabs.Group>
-    </div>
-  );
+import { Tabs, TabsRef } from "flowbite-react";
+import { Dispatch, SetStateAction, useRef, useState } from "react";
+import React from "react";
+
+
+interface NavbarProps {
+  activeTab: number;
+  setActiveTab: Dispatch<SetStateAction<number>>;
+  TabLabels: string[];
 }
+
+const Navbar = React.forwardRef<TabsRef, NavbarProps>((props, tabsRef) => {
+  const { setActiveTab, activeTab, TabLabels } = props;
+  return (
+    <Tabs.Group
+      ref={tabsRef}
+      onActiveTabChange={(tab) => { setActiveTab(tab) }}
+      className="justify-center flex-nowrap [& *]:text-fl-xs-2xl gap-fl-md [& *]:h-fl-2xs-2xl  [& *]:w-fl-2xl" aria-label="Full width tabs" style="default">
+      {
+        TabLabels.map((title, index) => (
+          < Tabs.Item active={activeTab === index} key={title} title={title}>
+          </Tabs.Item>))
+      }
+    </Tabs.Group >
+  );
+})
+
+export default Navbar;
+/*
+
+      <Tabs.Item title="Add New Task">
+        <NewTask />
+      </Tabs.Item>
+      <Tabs.Item title="Get Result">Get Result</Tabs.Item>
+      <Tabs.Item title="Verify Service">Verify Service</Tabs.Item>
+      <Tabs.Item title="Contact Us">
+        <ContactUs />
+      </Tabs.Item>
+      */
