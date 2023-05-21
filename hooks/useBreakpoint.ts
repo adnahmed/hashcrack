de4d17ae1441fa5a4df9e47649ec769657b1be56
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-05-21 13:10:06 +0500

    add another breakpoint

--- hooks/useBreakpoint.ts
+++ hooks/useBreakpoint.ts
@@ -1,6 +1,6 @@
 import { create } from "@kodingdotninja/use-tailwind-breakpoint";
 import { useEffect } from "react";
-type BreakpointKey = "sm" | "md" | "lg" | "xl" | "xl2";
+type BreakpointKey = "sm" | "md" | "lg" | "lg2" | "xl" | "xl2";
 
 const hooks = create({
   // Smartphone
@@ -9,6 +9,8 @@ const hooks = create({
   // Tablet
   lg: "768px",
   // Notebook
+  lg2: "940px",
+  // Laptop
   xl: "1024px",
   // Desktop
   xl2: "1600px",
