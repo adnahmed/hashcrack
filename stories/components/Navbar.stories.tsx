d73e19fba2a4e4f7efcf90d5fdac1f13d3082eb3
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-05-21 15:16:01 +0500

    add navbar props to story

--- stories/components/Navbar.stories.tsx
+++ stories/components/Navbar.stories.tsx
@@ -1,11 +1,15 @@
 import NavbarComponent from "@/components/Navbar";
+import { TabLabels } from "@/components/ui/Tabs";
 import type { Story, StoryDefault } from "@ladle/react";
+import { useState } from "react";
 
-export const NavBarPage: Story = () => (
-  <>
-    <NavbarComponent />
+export const NavBarPage: Story = () => {
+  const [activeTab, setActiveTab] = useState(0)
+  return (<>
+    <NavbarComponent TabLabels={TabLabels} activeTab={activeTab} setActiveTab={setActiveTab} />
   </>
-);
+  );
+}
 
 NavBarPage.storyName = "Navbar";
 
