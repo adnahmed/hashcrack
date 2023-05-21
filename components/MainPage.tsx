5d9c6cd40a8903477046883478dd5cbc3b27c013
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-05-21 16:35:34 +0500

    import tabs

--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -6,23 +6,10 @@ import { useTheme } from "next-themes";
 import Navbar from "./Navbar";
 import Title from "./Title";
 import { useBreakpoint } from "@/hooks/useBreakpoint";
-import { ReactNode, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { TabsRef } from "flowbite-react";
-import NewTask from "@/features/NewTask/NewTask";
-import ContactUs from "./ContactUs";
-import TaskQueue from "./TaskQueue";
-interface Tabs {
-  [key: string]: ReactNode
-}
+import { TabLabels, TabsEnum } from "./ui/Tabs";
 const MainPage = () => {
-  const TabsEnum: Tabs = {
-    "Tasks Queue": <TaskQueue />,
-    "Add New Tasks": <NewTask />,
-    "Get Result": <div></div>,
-    "Verify Service": <div></div>,
-    "Contact Us": <ContactUs />
-  }
-  const TabLabels = Object.keys(TabsEnum)
   const { theme, setTheme } = useTheme();
   const [openNavbar, setOpenNavbar] = useState(false);
   const [activeTab, setActiveTab] = useState<number>(0);
