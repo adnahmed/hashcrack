9f33d5c7ef5256aaff9e2a8a5d23e414bdec4b17
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-06-05 18:57:26 +0500

    fix style for wizard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import IntialCheck from "@/components/ui/InitialCheck";
 import Loading from "@/components/ui/Loading";
 import { selectCaptchaValidated } from "@/features/Captcha/captchaSlice";
-import styles from "@/styles/Home.module.css";
+import { selectActiveTab } from "@/features/Navigation/navigationSlice";
 import Head from "next/head";
 import { Suspense, lazy } from "react";
 import { useSelector } from "react-redux";
@@ -9,7 +9,8 @@ import { NextPageWithLayout } from "./_app";
 const MainPage = lazy(() => import("@/components/MainPage"));
 const Home: NextPageWithLayout = () => {
   const captchaValidated = useSelector(selectCaptchaValidated);
-
+  const activeTab = useSelector(selectActiveTab);
+  const isWizardTab = activeTab === 1;
   return (
     <>
       <Head>
@@ -18,17 +19,14 @@ const Home: NextPageWithLayout = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
-      <div>
-        <div className={styles.main}>
-          {/* TODO: Use Spinner */}
-          {!captchaValidated ? (
-            <IntialCheck />
-          ) : (
-            <Suspense fallback={<Loading />}>
-              <MainPage />
-            </Suspense>
-          )}
-        </div>
+      <div className={`${isWizardTab ? 'flex h-screen flex-col' : ''} `}>
+        {!captchaValidated ? (
+          <IntialCheck />
+        ) : (
+          <Suspense fallback={<Loading />}>
+            <MainPage />
+          </Suspense>
+        )}
       </div>
     </>
   );
