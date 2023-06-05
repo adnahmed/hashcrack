c48ce338dff4383ce74775cb1a8d3f12cb01fcc9
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-06-05 18:55:39 +0500

    mv Navbar to Navigation feature

--- features/Navigation/Navbar.tsx
+++ features/Navigation/Navbar.tsx
@@ -1,20 +1,22 @@
 import { Tabs, TabsRef } from "flowbite-react";
-import React, { Dispatch, SetStateAction } from "react";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { activeTabChanged } from "./navigationSlice";
 
 
 interface NavbarProps {
   activeTab: number;
-  setActiveTab: Dispatch<SetStateAction<number>>;
   TabLabels: string[];
 }
 
 const Navbar = React.forwardRef<TabsRef, NavbarProps>((props, tabsRef) => {
-  const { setActiveTab, activeTab, TabLabels } = props;
+  const { activeTab, TabLabels } = props;
+  const dispatch = useDispatch();
   return (
     <Tabs.Group
       ref={tabsRef}
-      onActiveTabChange={(tab) => { setActiveTab(tab) }}
-      className="[& *]:text-fl-xs-2xl [& *]:h-fl-2xs-2xl [& *]:w-fl-2xl  flex-nowrap justify-center " aria-label="Full width tabs" style="default">
+      onActiveTabChange={(tab) => { dispatch(activeTabChanged(tab)) }}
+      className=" flex-nowrap justify-center gap-0 " aria-label="Full width tabs" style="default">
       {
         TabLabels.map((title, index) => (
           <Tabs.Item active={activeTab === index} key={title} title={title}>
