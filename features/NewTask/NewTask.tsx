c42f56ff181c0af6a6c41addba4f270f5d912fae
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-06-10 14:22:36 +0500

    reformat newTask

--- features/NewTask/NewTask.tsx
+++ features/NewTask/NewTask.tsx
@@ -1,28 +1,40 @@
-import hashTypes from "@/assets/hash-types.json";
+import hashTypes from '@/assets/hash-types.json';
 import { Section, Step, WizardPage } from '@patternfly-labs/react-form-wizard';
-import { useState } from "react";
+import { useState } from 'react';
 import toast from 'react-hot-toast';
 import SelectSearch from 'react-select-search';
-import HashInput from "../HashInput/HashInput";
+import HashInput from '../HashInput/HashInput';
 export default function NewTask() {
-  const { options } = hashTypes;
-  const [hashType, setHashType] = useState<string>("-1");
-  return (
-    <WizardPage yaml={false} title="" onSubmit={function (data: unknown): Promise<void> {
-      throw new Error("Function not implemented.");
-    }} onCancel={function (): void {
-      throw new Error("Function not implemented.");
-    }}>
-      <Step label="Add Hashlist" id="add-hashlist">
-        <Section autohide={false} id="select-hashtype" label={"Select Hash Type"}>
-          <SelectSearch onChange={(selectedValue) => { setHashType(selectedValue.toString()); toast.error("hello") }} search options={options} placeholder="Choose Hash Type" />
-        </Section>
-        {hashType !== undefined && hashType !== "-1" && (
-          <Section autohide={false} id="input-hash" label={"Input Hashlist"}>
-            <HashInput hashType={hashType} />
-          </Section>
-        )}
-      </Step>
-    </WizardPage>
-  )
+    const { options } = hashTypes;
+    const [hashType, setHashType] = useState<string>('-1');
+    return (
+        <WizardPage
+            yaml={false}
+            title=""
+            onSubmit={function (data: unknown): Promise<void> {
+                throw new Error('Function not implemented.');
+            }}
+            onCancel={function (): void {
+                throw new Error('Function not implemented.');
+            }}>
+            <Step label="Add Hashlist" id="add-hashlist">
+                <Section autohide={false} id="select-hashtype" label={'Select Hash Type'}>
+                    <SelectSearch
+                        onChange={(selectedValue) => {
+                            setHashType(selectedValue.toString());
+                            toast.error('hello');
+                        }}
+                        search
+                        options={options}
+                        placeholder="Choose Hash Type"
+                    />
+                </Section>
+                {hashType !== undefined && hashType !== '-1' && (
+                    <Section autohide={false} id="input-hash" label={'Input Hashlist'}>
+                        <HashInput hashType={hashType} />
+                    </Section>
+                )}
+            </Step>
+        </WizardPage>
+    );
 }
