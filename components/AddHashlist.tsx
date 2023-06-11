a3accd707d5195969cf9cb5dc612b7ef8961f13e
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-06-11 16:17:17 +0500

    mv addhashlist step into component

---
+++ components/AddHashlist.tsx
@@ -0,0 +1,28 @@
+import hashTypes from '@/assets/hash-types.json';
+import HashInput from '@/features/HashInput/HashInput';
+import { selectSelectedHashType, selectedHashType } from '@/features/NewTask/newTaskSlice';
+import { Section } from '@patternfly-labs/react-form-wizard';
+import { useDispatch, useSelector } from 'react-redux';
+import SelectSearch from 'react-select-search';
+function AddHashlist() {
+    const dispatch = useDispatch();
+    const hashtype = useSelector(selectSelectedHashType);
+    const { options } = hashTypes;
+    return (
+        <>
+            <Section autohide={false} id="select-hashtype" label={'Select Hash Type'}>
+                <SelectSearch
+                    onChange={(selectedValue) => {
+                        dispatch(selectedHashType(selectedValue.toString()));
+                    }}
+                    search
+                    options={options}
+                    placeholder="Choose Hash Type"
+                />
+            </Section>
+            {hashtype !== undefined && hashtype !== '-1' && <HashInput hashType={hashtype} />}
+        </>
+    );
+}
+
+export default AddHashlist;
--- features/NewTask/NewTask.tsx
+++ features/NewTask/NewTask.tsx
@@ -1,13 +1,6 @@
-import hashTypes from '@/assets/hash-types.json';
-import { Section, Step, WizardPage } from '@patternfly-labs/react-form-wizard';
-import { useDispatch, useSelector } from 'react-redux';
-import SelectSearch from 'react-select-search';
-import HashInput from '../HashInput/HashInput';
-import { selectSelectedHashType, selectedHashType } from './newTaskSlice';
+import AddHashlist from '@/components/AddHashlist';
+import { Step, WizardPage } from '@patternfly-labs/react-form-wizard';
 export default function NewTask() {
-    const { options } = hashTypes;
-    const dispatch = useDispatch();
-    const hashtype = useSelector(selectSelectedHashType);
     return (
         <WizardPage
             yaml={false}
@@ -19,24 +12,9 @@ export default function NewTask() {
                 throw new Error('Function not implemented.');
             }}>
             <Step label="Add Hashlist" id="add-hashlist">
-                <Section autohide={false} id="select-hashtype" label={'Select Hash Type'}>
-                    <SelectSearch
-                        onChange={(selectedValue) => {
-                            dispatch(selectedHashType(selectedValue.toString()));
-                        }}
-                        search
-                        options={options}
-                        placeholder="Choose Hash Type"
-                    />
-                </Section>
-                {hashtype !== undefined && hashtype !== '-1' && <HashInput hashType={hashtype} />}
+                <AddHashlist />
             </Step>
-            <Step
-                hidden={(item) => {
-                    return true;
-                }}
-                label={'Verify Hashlist'}
-                id={'verify-hashlist'}></Step>
+            <Step label={'Verify Hashlist'} id={'verify-hashlist'}></Step>
         </WizardPage>
     );
 }
