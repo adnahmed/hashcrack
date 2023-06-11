2cffa61f8c1082da23cfcd9f49abbab4f154eaf6
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-06-11 21:16:56 +0500

    wizardStepReached and onNext and wip verificationStatus

--- features/NewTask/NewTask.tsx
+++ features/NewTask/NewTask.tsx
@@ -1,10 +1,47 @@
 import AddHashlist from '@/components/AddHashlist';
-import { Wizard } from '@patternfly/react-core';
-const steps = [{ name: 'Add Hashlist', component: <AddHashlist /> }];
+import { Wizard, WizardStep } from '@patternfly/react-core';
+import { useDispatch, useSelector } from 'react-redux';
+import { initiatedVerifyHashList, selectVerificationState, selectWizardStepReached, stepIdReached } from './newTaskSlice';
 export default function NewTask() {
+    const wizardStepReached = useSelector(selectWizardStepReached);
+    const verificationSatus = useSelector(selectVerificationState);
+    const dispatch = useDispatch();
+    const steps: WizardStep[] = (() => {
+        let steps = [
+            { name: 'Add Hashlist', component: <AddHashlist /> },
+            { name: 'Verify Hashlist', component: <AddHashlist /> },
+        ];
+        let id = 1;
+        steps = steps.map((step) => ({
+            ...step,
+            id: id++,
+            canJumpTo: id > 1 ? wizardStepReached : null,
+        }));
+        return steps;
+    })();
+    const closeWizard = () => {
+        // eslint-disable-next-line no-console
+        console.log('close wizard');
+    };
+    const onNext = ({ id }: WizardStep) => {
+        if (id) {
+            if (typeof id === 'string') {
+                id = parseInt(id);
+            }
+            if (id === 2) {
+                dispatch(initiatedVerifyHashList());
+            }
+            dispatch(stepIdReached(wizardStepReached < id ? id : wizardStepReached));
+        }
+    };
+
     return (
         <Wizard
+            hideClose={true}
+            cancelButtonText=""
             title=""
+            onNext={onNext}
+            onClose={closeWizard}
             onSubmit={function (data: unknown): Promise<void> {
                 throw new Error('Function not implemented.');
             }}
--- features/NewTask/newTaskSlice.ts
+++ features/NewTask/newTaskSlice.ts
@@ -3,16 +3,26 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { apiSlice } from "../api/apiSlice";
 interface NewTaskState {
     selectedHashType: string;
+    wizardStepIdReached: number;
+    verificationState: "verifying" | "verified" | "unverified";
 }
 const newTask = createSlice({
     name: "newTask",
     initialState: {
-        selectedHashType: '-1'
+        selectedHashType: '-1',
+        wizardStepIdReached: 1,
+        verificationState: "unverified",
     } as NewTaskState,
     reducers: {
         selectedHashType: (state, action: PayloadAction<string>) => {
             state.selectedHashType = action.payload;
         },
+        stepIdReached: (state, action: PayloadAction<number>) => {
+            state.wizardStepIdReached = action.payload;
+        },
+        initiatedVerifyHashList: (state, action: PayloadAction<NewTaskState['verificationState']>) => {
+            state.verificationState = action.payload;
+        }
     },
 });
 
@@ -25,7 +35,8 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
 })
 
 export default newTask;
-export const selectSelectedHashType = (state: AppState) =>
-    state.newTask.selectedHashType;
-export const { selectedHashType } = newTask.actions;
+export const selectSelectedHashType = (state: AppState) => state.newTask.selectedHashType;
+export const selectWizardStepReached = (state: AppState) => state.newTask.wizardStepIdReached;
+export const selectVerificationState = (state: AppState) => state.newTask.verificationState;
+export const { selectedHashType, stepIdReached, initiatedVerifyHashList } = newTask.actions;
 export const { useNewTaskMutation } = extendedApiSlice
