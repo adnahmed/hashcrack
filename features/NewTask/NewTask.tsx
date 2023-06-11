11ebaa40c14ce477108215f9afdc2462e8b3206b
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-06-11 16:01:07 +0500

    use newTask slice

--- features/NewTask/NewTask.tsx
+++ features/NewTask/NewTask.tsx
@@ -1,11 +1,13 @@
 import hashTypes from '@/assets/hash-types.json';
 import { Section, Step, WizardPage } from '@patternfly-labs/react-form-wizard';
-import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import SelectSearch from 'react-select-search';
 import HashInput from '../HashInput/HashInput';
+import { selectSelectedHashType, selectedHashType } from './newTaskSlice';
 export default function NewTask() {
     const { options } = hashTypes;
-    const [hashType, setHashType] = useState<string>('-1');
+    const dispatch = useDispatch();
+    const hashtype = useSelector(selectSelectedHashType);
     return (
         <WizardPage
             yaml={false}
@@ -20,15 +22,21 @@ export default function NewTask() {
                 <Section autohide={false} id="select-hashtype" label={'Select Hash Type'}>
                     <SelectSearch
                         onChange={(selectedValue) => {
-                            setHashType(selectedValue.toString());
+                            dispatch(selectedHashType(selectedValue.toString()));
                         }}
                         search
                         options={options}
                         placeholder="Choose Hash Type"
                     />
                 </Section>
-                {hashType !== undefined && hashType !== '-1' && <HashInput hashType={hashType} />}
+                {hashtype !== undefined && hashtype !== '-1' && <HashInput hashType={hashtype} />}
             </Step>
+            <Step
+                hidden={(item) => {
+                    return true;
+                }}
+                label={'Verify Hashlist'}
+                id={'verify-hashlist'}></Step>
         </WizardPage>
     );
 }
