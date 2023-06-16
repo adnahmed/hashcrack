9fd99aa9801a8dc1c8b264f6d64c8b5e82635fdf
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-06-17 00:00:22 +0500

    add kaitai and typings

---
+++ kaitai-struct/KaitaiStream/index.d.ts
@@ -0,0 +1,7 @@
+import KataiStream from 'kaitai-struct/KaitaiStream';
+declare module 'KaitaiStreams';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface KataiStream { }
+
+export default KaitaiStream;
