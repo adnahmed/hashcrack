908fcafebbec4bc0642045c21faddf8b3b3c2bd4
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-06-16 16:57:16 +0500

    pass context in newtask tab

--- components/ui/Tabs.tsx
+++ components/ui/Tabs.tsx
@@ -1,17 +1,26 @@
-import NewTask from "@/features/NewTask/NewTask";
-import { ReactNode } from "react";
-import TaskQueue from "../TaskQueue";
-import ContactUs from "../ContactUs";
+import HashlistContext from '@/Context/HashlistContext';
+import NewTask from '@/features/NewTask/NewTask';
+import { ReactNode, useState } from 'react';
+import ContactUs from '../ContactUs';
+import TaskQueue from '../TaskQueue';
 
 export interface Tabs {
-  [key: string]: ReactNode
+    [key: string]: ReactNode;
 }
 
+const NewTaskWrapper = (props: any) => {
+    const [hashlist, setHashlist] = useState('');
+    return (
+        <HashlistContext.Provider value={{ hashlist, setHashlist }}>
+            <NewTask />
+        </HashlistContext.Provider>
+    );
+};
 export const TabsEnum: Tabs = {
-  "Tasks Queue": <TaskQueue />,
-  "Add New Tasks": <NewTask />,
-  "Get Result": <div></div>,
-  "Verify Service": <div></div>,
-  "Contact Us": <ContactUs />
-}
-export const TabLabels = Object.keys(TabsEnum)
+    'Tasks Queue': <TaskQueue />,
+    'Add New Tasks': <NewTaskWrapper />,
+    'Get Result': <div></div>,
+    'Verify Service': <div></div>,
+    'Contact Us': <ContactUs />,
+};
+export const TabLabels = Object.keys(TabsEnum);
