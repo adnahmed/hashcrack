8a7e575abf965dd6b510719cce4b7f4669fb614d
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-06-17 10:42:49 +0500

    onDrop handler and hashlist reading

--- features/HashInput/HashTypes/All.tsx
+++ features/HashInput/HashTypes/All.tsx
@@ -1,15 +1,37 @@
+import HashlistContext from '@/Context/HashlistContext';
 import hashTypes from '@/assets/hash-types.json';
+import { verifyHashlist } from '@/features/NewTask/newTaskSlice';
+import { useAppDispatch } from '@/lib/redux/store';
 import clsx from 'clsx';
-import { useDropzone } from 'react-dropzone';
+import Head from 'next/head';
+import Script from 'next/script';
+import { useCallback, useContext } from 'react';
+import { DropzoneOptions, useDropzone } from 'react-dropzone';
 import { HashInputInstructions, HashInputProps } from '../HashInput';
+
 const AllHashTypes: React.FunctionComponent<HashInputProps> = ({ hashType }) => {
     const { options } = hashTypes;
+    const dispatch = useAppDispatch();
     const wirelessNetworkGroup = options.find((p) => /wireless networks/i.test(p['name']));
     const isWireless = wirelessNetworkGroup?.items.find((p) => p['value'] === hashType);
-    const { acceptedFiles, getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone();
+    const hashlistConsumer = useContext(HashlistContext);
+    const onDrop = useCallback<NonNullable<DropzoneOptions['onDrop']>>(async (acceptedFiles) => {
+        try {
+            const resp = await dispatch(verifyHashlist({ inputMethod: 'file', hashlistFile: acceptedFiles[0] })).unwrap();
+            console.log(resp);
+        } catch (err) {
+            console.error(err);
+        }
+    }, []);
+    const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone({ onDrop });
     return !isWireless ? (
         <>
-            <textarea id="hashlist" name="hashlist" className="h-32 w-full resize-none rounded border border-gray-300 bg-gray-100 bg-opacity-50 px-3 py-1 text-base leading-6 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"></textarea>
+            <Head>
+                <title>Add Hashlist</title>
+            </Head>
+            <Script src="/brython_stdlib.js" strategy="afterInteractive" onLoad={() => console.log('loaded brython_stdlib')} />
+            <Script src="/brython.js" strategy="afterInteractive" onLoad={() => console.log('loaded brython')} />
+            {hashlistConsumer !== null && <textarea id="hashlist" name="hashlist" onChange={(e) => hashlistConsumer.setHashlist(e.target.value)} value={hashlistConsumer.hashlist} className="h-32 w-full resize-none rounded border border-gray-300 bg-gray-100 bg-opacity-50 px-3 py-1 text-base leading-6 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"></textarea>}
             <label htmlFor="hashlist" className="mx-auto text-sm leading-7 text-gray-600">
                 Paste your hashlist here
                 <span className="text-red-500">*</span>
