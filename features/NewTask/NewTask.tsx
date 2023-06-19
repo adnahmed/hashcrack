6e561e8a314824470657d7a5fa74185da1358be3
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-06-19 10:39:35 +0500

    update newtask component to change steps on verification start

--- features/NewTask/NewTask.tsx
+++ features/NewTask/NewTask.tsx
@@ -1,62 +1,114 @@
 import HashlistContext from '@/Context/HashlistContext';
 import AddHashlist from '@/components/AddHashlist';
 import { useAppDispatch, useAppSelector } from '@/lib/redux/store';
-import { Wizard, WizardStep } from '@patternfly/react-core';
-import { useContext, useEffect } from 'react';
+import { Button, Wizard, WizardContextConsumer, WizardFooter, WizardStep } from '@patternfly/react-core';
+import { useContext, useEffect, useMemo } from 'react';
 import VerifyHashlist from '../VerifyHashlist/VerifyHashlist';
-import { selectHashlistVerified, selectWizardStepReached, stepIdReached, verifyHashlist } from './newTaskSlice';
+import { selectWizardStepReached, stepIdReached } from '../Wizard/wizardSlice';
+import { selectHashlistFile, selectHashlistVerified, selectSelectedHashType, selectVerifyingHashlist, verifyHashlist } from './newTaskSlice';
 
 export default function NewTask() {
     const wizardStepReached = useAppSelector(selectWizardStepReached);
+    const selectedHashType = useAppSelector(selectSelectedHashType);
     const hashlistVerified = useAppSelector(selectHashlistVerified);
+    const verifyingHashlist = useAppSelector(selectVerifyingHashlist);
+    const hashlistFile = useAppSelector(selectHashlistFile);
     const hashlistConsumer = useContext(HashlistContext);
+    const usingTextArea = hashlistConsumer && hashlistConsumer.hashlist.length !== 0;
+    const VerifyStepDisabled = useMemo(() => (hashlistFile === undefined && !usingTextArea) || selectedHashType === '-1', [hashlistFile, usingTextArea, selectedHashType]);
     const dispatch = useAppDispatch();
+    const steps: WizardStep[] = useMemo(
+        () => [
+            {
+                id: '1',
+                name: 'Add Hashlist',
+                component: <AddHashlist />,
+                isDisabled: verifyingHashlist || hashlistVerified,
+            },
+            {
+                id: '2',
+                name: 'Verify Hashlist',
+                component: <VerifyHashlist />,
+                isDisabled: VerifyStepDisabled,
+                canJumpTo: wizardStepReached === 2,
+            },
+        ],
+        [hashlistFile, hashlistVerified, selectedHashType, usingTextArea, verifyingHashlist, wizardStepReached]
+    );
     useEffect(() => {
-        console.log(wizardStepReached);
-    }, [wizardStepReached]);
-
-    const steps: WizardStep[] = (() => {
-        let steps = [
-            { name: 'Add Hashlist', component: <AddHashlist /> },
-            { name: 'Verify Hashlist', component: <VerifyHashlist />, isDisabled: false },
-        ];
-        let id = 1;
-        steps = steps.map((step) => ({
-            ...step,
-            id: id++,
-            canJumpTo: id > 1 ? wizardStepReached : null,
-        }));
-        return steps;
-    })();
-
+        console.log(steps);
+    }, [steps]);
     const closeWizard = () => {
         // eslint-disable-next-line no-console
         console.log('try me');
     };
 
-    const onNext = ({ id }: WizardStep) => {
+    const onNext = async ({ id }: WizardStep) => {
         if (id) {
-            console.log(id);
             if (typeof id === 'string') {
                 id = parseInt(id);
             }
+            const nextStep = (id: number) => dispatch(stepIdReached(wizardStepReached < id ? id : wizardStepReached));
             if (id === 2) {
-                if (hashlistConsumer !== null) dispatch(verifyHashlist({ inputMethod: 'textarea', hashlist: hashlistConsumer.hashlist.split('\n') }));
+                if (usingTextArea) {
+                    dispatch(verifyHashlist({ inputMethod: 'textarea', hashlist: hashlistConsumer.hashlist.split('\n') }));
+                }
+                if (hashlistFile) {
+                    try {
+                        const resp = await dispatch(verifyHashlist({ inputMethod: 'file' })).unwrap();
+                        console.log(resp);
+                    } catch (err) {
+                        console.error(err);
+                    }
+                }
+                if (usingTextArea || hashlistFile) {
+                    nextStep(id);
+                }
+            } else {
             }
-            dispatch(stepIdReached(wizardStepReached < id ? id : wizardStepReached));
         }
     };
 
+    const onReset = ({ id }: WizardStep) => {
+        // TODO: Add reset reducer
+    };
+
+    const CustomFooter = (
+        <WizardFooter>
+            <WizardContextConsumer>
+                {({ activeStep, goToStepByName, onNext, onBack, onClose }) => {
+                    if (activeStep.id === '1') {
+                        return (
+                            <>
+                                <Button variant="primary" className={VerifyStepDisabled ? 'pf-m-disabled' : ''} type="submit" onClick={onNext}>
+                                    Next
+                                </Button>
+                            </>
+                        );
+                    }
+                    return (
+                        <>
+                            {/* <Button onClick={() => validateLastStep(onNext)}>Validate</Button> */}
+                            {/* <Button onClick={() => goToStepByName('First step')}>Go to Beginning</Button> */}
+                        </>
+                    );
+                }}
+            </WizardContextConsumer>
+        </WizardFooter>
+    );
     return (
         <Wizard
             hideClose={true}
             cancelButtonText=""
             title=""
             onNext={onNext}
+            onBack={onReset}
+            footer={CustomFooter}
             onClose={closeWizard}
             onSubmit={function (data: unknown): Promise<void> {
                 throw new Error('Function not implemented.');
             }}
+            backButtonText={'Reset'}
             steps={steps}
         />
     );
