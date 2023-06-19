70202fc66a1f0eab057c05d6d7a1b98c73f4dbfa
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-06-19 10:35:14 +0500

    reformat and add react-persist

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -1,39 +1,62 @@
+import { apiSlice } from "@/features/api/apiSlice";
 import captcha, { extendedApiSlice } from "@/features/Captcha/captchaSlice";
 import navigation from "@/features/Navigation/navigationSlice";
 import newTask from "@/features/NewTask/newTaskSlice";
-import { apiSlice } from "@/features/api/apiSlice";
-import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit";
+import wizard from "@/features/Wizard/wizardSlice";
+import { Action, combineReducers, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { FLUSH, PAUSE, PERSIST, persistStore, PURGE, REGISTER, REHYDRATE } from "redux-persist";
+import persistReducer from "redux-persist/es/persistReducer";
+import storage from 'redux-persist/lib/storage/session';
 import thunkMiddleware from 'redux-thunk';
 import { rtkQueryErrorLogger } from "./errorLoggerMiddleware";
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+}
 
-export function makeStore() {
+const rootReducer = combineReducers({
+  [extendedApiSlice.reducerPath]: extendedApiSlice.reducer,
+  captcha: captcha.reducer,
+  navigation: navigation.reducer,
+  newTask: newTask.reducer,
+  wizard: wizard.reducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// TODO: add next-redux-wrapper hydration.
+const makeStore = () => {
   return configureStore({
-    reducer: {
-      [extendedApiSlice.reducerPath]: extendedApiSlice.reducer,
-      captcha: captcha.reducer,
-      navigation: navigation.reducer,
-      newTask: newTask.reducer,
-    },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }).concat(
         apiSlice.middleware,
         rtkQueryErrorLogger,
-        thunkMiddleware
+        thunkMiddleware,
       ),
     devTools: process.env.NODE_ENV === "development",
   });
 }
+
 const store = makeStore();
+
 export type AppStore = ReturnType<typeof makeStore>;
 export type AppState = ReturnType<AppStore["getState"]>;
-export type AppDispatch = typeof store.dispatch;
-export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
+export type AppDispatch = AppStore['dispatch'];
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   AppState,
   unknown,
   Action<string>
 >;
+
 export default store;
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
+export const persistor = persistStore(store);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,45 +1,45 @@
-import store from "@/lib/redux/store";
-import "@/styles/globals.scss";
-import localFont from "@next/font/local";
-import "@total-typescript/ts-reset";
-import { NextPage } from "next";
-import { ThemeProvider } from "next-themes";
-import type { AppProps, NextWebVitalsMetric } from "next/app";
-import { ReactElement, ReactNode } from "react";
-import { Provider } from "react-redux";
-import ErrorBoundary from "../components/ErrorBoundary";
+import store from '@/lib/redux/store';
+import '@/styles/globals.scss';
+import localFont from '@next/font/local';
+import '@total-typescript/ts-reset';
+import { NextPage } from 'next';
+import { ThemeProvider } from 'next-themes';
+import type { AppProps, NextWebVitalsMetric } from 'next/app';
+import { ReactElement, ReactNode } from 'react';
+import { Provider } from 'react-redux';
+import ErrorBoundary from '../components/ErrorBoundary';
 
 // Font files can be colocated inside of `app`
 export const inter = localFont({
-  src: "../assets/inter.woff2",
-  display: "swap",
+    src: '../assets/inter.woff2',
+    display: 'swap',
 });
 
 export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
+    getLayout?: (page: ReactElement) => ReactNode;
 };
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
+    Component: NextPageWithLayout;
 };
 
 export function reportWebVitals(metric: NextWebVitalsMetric) {
-  // TODO: Report metrics to Analytics.
-  console.log(metric);
+    // TODO: Report metrics to Analytics.
+    console.log(metric);
 }
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout || ((page) => page);
-  return (
-    <Provider store={store}>
-      <ErrorBoundary>
-        {getLayout(
-          <ThemeProvider attribute="class">
-            <Component {...pageProps} />
-          </ThemeProvider>
-        )}
-      </ErrorBoundary>
-    </Provider >
-  );
+    // Use the layout defined at the page level, if available
+    const getLayout = Component.getLayout || ((page) => page);
+    return (
+        <Provider store={store}>
+            <ErrorBoundary>
+                {getLayout(
+                    <ThemeProvider attribute="class">
+                        <Component {...pageProps} />
+                    </ThemeProvider>
+                )}
+            </ErrorBoundary>
+        </Provider>
+    );
 }
