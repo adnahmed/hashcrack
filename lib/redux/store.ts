c29ea3d6243e2cff6a471b4156112cb0653474ee
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-06-20 14:35:46 +0500

    remove all traces of react-persist

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -3,48 +3,50 @@ import captcha, { extendedApiSlice } from "@/features/Captcha/captchaSlice";
 import navigation from "@/features/Navigation/navigationSlice";
 import newTask from "@/features/NewTask/newTaskSlice";
 import wizard from "@/features/Wizard/wizardSlice";
-import { Action, combineReducers, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import { Action, AnyAction, CombinedState, combineReducers, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import { createWrapper, HYDRATE } from 'next-redux-wrapper';
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { FLUSH, PAUSE, PERSIST, persistStore, PURGE, REGISTER, REHYDRATE } from "redux-persist";
-import persistReducer from "redux-persist/es/persistReducer";
-import storage from 'redux-persist/lib/storage/session';
 import thunkMiddleware from 'redux-thunk';
 import { rtkQueryErrorLogger } from "./errorLoggerMiddleware";
-const persistConfig = {
-  key: "root",
-  version: 1,
-  storage,
-}
 
-const rootReducer = combineReducers({
+
+const combinedReducer = combineReducers({
   [extendedApiSlice.reducerPath]: extendedApiSlice.reducer,
   captcha: captcha.reducer,
   navigation: navigation.reducer,
   newTask: newTask.reducer,
   wizard: wizard.reducer,
-})
+});
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// TODO: add next-redux-wrapper hydration.
-const makeStore = () => {
-  return configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
-        serializableCheck: {
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-      }).concat(
-        apiSlice.middleware,
-        rtkQueryErrorLogger,
-        thunkMiddleware,
-      ),
-    devTools: process.env.NODE_ENV === "development",
-  });
+const rootReducer = (state: any, action: AnyAction) => {
+  if (action.type === HYDRATE) {
+    return {
+      ...state,
+      ...action.payload,
+    };
+  } else
+    return combinedReducer(state, action)
 }
 
-const store = makeStore();
+const makeConfiguredStore = (reducer: CombinedState<typeof rootReducer>) => configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [],
+      },
+    }).concat(
+      apiSlice.middleware,
+      rtkQueryErrorLogger,
+      thunkMiddleware,
+    ),
+  devTools: process.env.NODE_ENV === "development",
+});
+
+
+const makeStore = () => {
+  return makeConfiguredStore(rootReducer);
+}
 
 export type AppStore = ReturnType<typeof makeStore>;
 export type AppState = ReturnType<AppStore["getState"]>;
@@ -56,7 +58,7 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   Action<string>
 >;
 
-export default store;
+const wrapper = createWrapper<AppState>(makeStore, { debug: process.env.NODE_ENV === "development" });
+export default wrapper;
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
-export const persistor = persistStore(store);
