40251111dc96692ce7701931052c5b6bc17e4986
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-06-25 22:51:44 +0500

    update HashlistDnD to store type, url and size

--- components/HashlistDnD.tsx
+++ components/HashlistDnD.tsx
@@ -3,32 +3,39 @@ import { WPACaptureFileTypes } from '@/features/HashInput/HashTypes/Wireless/EAP
 import { selectedHashlistFile } from '@/features/NewTask/newTaskSlice';
 import { useAppDispatch } from '@/lib/redux/store';
 import clsx from 'clsx';
-import { useCallback, useContext } from 'react';
-import { DropzoneOptions, useDropzone } from 'react-dropzone';
+import { useContext } from 'react';
+import { FileWithPath, useDropzone } from 'react-dropzone';
+import toast from 'react-hot-toast';
 interface HashlistDnDProps {
     wireless?: boolean;
 }
 const HashlistDnD = ({ wireless = false }: HashlistDnDProps) => {
     const dispatch = useAppDispatch();
-
     const hashlistConsumer = useContext(HashlistContext);
     const usingTextArea = hashlistConsumer !== null && hashlistConsumer.hashlist.length !== 0;
-    const onDrop = useCallback<NonNullable<DropzoneOptions['onDrop']>>(async (acceptedFiles) => {
-        dispatch(selectedHashlistFile(acceptedFiles[0]));
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    const acceptedFiles = wireless ?  {
+            'application/octet-stream': WPACaptureFileTypes,
+    } : undefined;
 
     const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone({
-        onDrop,
+        onDrop: (acceptedFiles: FileWithPath[]) => {
+        if (acceptedFiles.length === 0) return;
+        const accFile = acceptedFiles[0];
+        const type = accFile.path?.split('.')?.pop();
+        if (!type) {
+            toast.error('Could not detect file type')
+            return;
+        }
+        dispatch(selectedHashlistFile( { dataUrl: URL.createObjectURL(accFile), type, size: accFile.size} ));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },
         maxFiles: 1,
-        accept: {
-            'application/octet-stream': WPACaptureFileTypes,
-        },
+        accept: acceptedFiles,
         noClick: usingTextArea,
     });
     return (
         <div {...getRootProps({ className: clsx({ dropzone: true, 'border-gray-500': true, 'bg-green-100': isDragReject, 'border-green-300': isDragReject }) })}>
-            <input name="capFile" {...getInputProps()} />
+            <input name="hashlistFile" {...getInputProps()} />
             <p className="dropzone-p">{!isDragActive ? (wireless ? 'Drag here .hccap, .hccapx, .cap, .pcap with WPA handshake or click to browse' : 'Drag here hashlist file or click to browse') : 'Drop file here...'}</p>
         </div>
     );
