fbfbde7539282595dd4e80000aa3b1b11cabb009
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-06-25 22:53:12 +0500

    update makeStore to use configureStore

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -3,49 +3,38 @@ import captcha, { extendedApiSlice } from "@/features/Captcha/captchaSlice";
 import navigation from "@/features/Navigation/navigationSlice";
 import newTask from "@/features/NewTask/newTaskSlice";
 import wizard from "@/features/Wizard/wizardSlice";
-import { Action, AnyAction, CombinedState, combineReducers, configureStore, ThunkAction } from "@reduxjs/toolkit";
-import { createWrapper, HYDRATE } from 'next-redux-wrapper';
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import { createWrapper } from "next-redux-wrapper";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import thunkMiddleware from 'redux-thunk';
 import { rtkQueryErrorLogger } from "./errorLoggerMiddleware";
 
-
-const combinedReducer = combineReducers({
-  [extendedApiSlice.reducerPath]: extendedApiSlice.reducer,
-  captcha: captcha.reducer,
-  navigation: navigation.reducer,
-  newTask: newTask.reducer,
-  wizard: wizard.reducer,
-});
-
-const rootReducer = (state: any, action: AnyAction) => {
-  if (action.type === HYDRATE) {
-    return {
-      ...state,
-      ...action.payload,
-    };
-  } else
-    return combinedReducer(state, action)
-}
-
-const makeConfiguredStore = (reducer: CombinedState<typeof rootReducer>) => configureStore({
-  reducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [],
-      },
-    }).concat(
-      apiSlice.middleware,
-      rtkQueryErrorLogger,
-      thunkMiddleware,
-    ),
-  devTools: process.env.NODE_ENV === "development",
-});
-
-
-export const makeStore = () => {
-  return makeConfiguredStore(rootReducer);
+export function makeStore() {
+  return configureStore({
+    reducer: {
+      'HYDRATE': (state: object, action) => ({
+        ...state,
+        ...action.payload,
+      })
+      ,
+      [extendedApiSlice.reducerPath]: extendedApiSlice.reducer,
+      captcha: captcha.reducer,
+      navigation: navigation.reducer,
+      newTask: newTask.reducer,
+      wizard: wizard.reducer
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [],
+        },
+      }).concat(
+        apiSlice.middleware,
+        rtkQueryErrorLogger,
+        thunkMiddleware,
+      ),
+    devTools: process.env.NODE_ENV === "development",
+  });
 }
 
 export type AppStore = ReturnType<typeof makeStore>;
@@ -58,7 +47,7 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   Action<string>
 >;
 
-const wrapper = createWrapper<AppState>(makeStore, { debug: process.env.NODE_ENV === "development" });
+const wrapper = createWrapper<AppStore>(makeStore, { debug: process.env.NODE_ENV === "development" });
 export default wrapper;
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
