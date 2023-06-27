b0252b9854389151d05b68f33bdf558bf9f2c384
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-06-27 12:43:08 +0500

    fix styling of hashlist dnd, text and searc

--- components/HashlistDnD.tsx
+++ components/HashlistDnD.tsx
@@ -3,7 +3,7 @@ import { WPACaptureFileTypes } from '@/features/HashInput/HashTypes/Wireless/EAP
 import { selectedHashlistFile } from '@/features/NewTask/newTaskSlice';
 import { useAppDispatch } from '@/lib/redux/store';
 import clsx from 'clsx';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { FileWithPath, useDropzone } from 'react-dropzone';
 import toast from 'react-hot-toast';
 interface HashlistDnDProps {
@@ -12,31 +12,46 @@ interface HashlistDnDProps {
 const HashlistDnD = ({ wireless = false }: HashlistDnDProps) => {
     const dispatch = useAppDispatch();
     const hashlistConsumer = useContext(HashlistContext);
+    const [currentHashlistFile, setCurrentHashlistFile] = useState<FileWithPath | undefined>();
+    const [currentLabel, setCurrentLabel] = useState<string | undefined>();
     const usingTextArea = hashlistConsumer !== null && hashlistConsumer.hashlist.length !== 0;
-    const acceptedFiles = wireless ?  {
-            'application/octet-stream': WPACaptureFileTypes,
+    const acceptedFiles = wireless ? {
+        'application/octet-stream': WPACaptureFileTypes,
     } : undefined;
-
     const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone({
         onDrop: (acceptedFiles: FileWithPath[]) => {
-        if (acceptedFiles.length === 0) return;
-        const accFile = acceptedFiles[0];
-        const type = accFile.path?.split('.')?.pop();
-        if (!type) {
-            toast.error('Could not detect file type')
-            return;
-        }
-        dispatch(selectedHashlistFile( { dataUrl: URL.createObjectURL(accFile), type, size: accFile.size} ));
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    },
+            if (acceptedFiles.length === 0) return;
+            const accFile = acceptedFiles[0];
+            const type = accFile.path?.split('.')?.pop();
+            if (!type) {
+                toast.error('Could not detect file type')
+                return;
+            }
+            setCurrentHashlistFile(accFile);
+            dispatch(selectedHashlistFile({ dataUrl: URL.createObjectURL(accFile), type, size: accFile.size }));
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+        },
         maxFiles: 1,
         accept: acceptedFiles,
         noClick: usingTextArea,
     });
+    useEffect(() => {
+        if (isDragActive) {
+            setCurrentLabel('Drop file here...')
+            return;
+        }
+        if (currentHashlistFile) {
+            setCurrentLabel(currentHashlistFile.name);
+            return;
+        }
+        if (wireless)
+            setCurrentLabel('Drag here .hccap, .hccapx, .cap, .pcap with WPA handshake or click to browse')
+        setCurrentLabel('Drag here hashlist file or click to browse')
+    }, [currentHashlistFile, isDragActive, wireless])
     return (
-        <div {...getRootProps({ className: clsx({ dropzone: true, 'border-gray-500': true, 'bg-green-100': isDragReject, 'border-green-300': isDragReject }) })}>
+        <div {...getRootProps({ className: clsx({ dropzone: true, 'border-gray-500': true, 'bg-green-100': isDragReject, 'border-green-300': isDragReject, 'border-blue-500': isDragActive, 'bg-blue-100': isDragActive, 'bg-[var(--theme-bg)]': currentHashlistFile, 'border-[var(--theme)]': currentHashlistFile }) })}>
             <input name="hashlistFile" {...getInputProps()} />
-            <p className="dropzone-p">{!isDragActive ? (wireless ? 'Drag here .hccap, .hccapx, .cap, .pcap with WPA handshake or click to browse' : 'Drag here hashlist file or click to browse') : 'Drop file here...'}</p>
+            <p className={`dropzone-p ${currentHashlistFile ? 'text-fl-lg text-[var(--theme)] ' : 'text-fl-sm md:text-fl-base'}`}>{currentLabel}</p>
         </div>
     );
 };
--- components/HashlistText.tsx
+++ components/HashlistText.tsx
@@ -2,6 +2,7 @@ import HashlistContext from '@/Context/HashlistContext';
 import { selectVerifyingHashlist } from '@/features/NewTask/newTaskSlice';
 import { useAppSelector } from '@/lib/redux/store';
 import { useContext } from 'react';
+import { selectHashlistFile } from '../features/NewTask/newTaskSlice';
 
 interface HashlistTextProps {
     wireless?: boolean;
@@ -9,17 +10,18 @@ interface HashlistTextProps {
 }
 function HashlistText({ wireless = false, isPMKID = false }: HashlistTextProps) {
     const hashlistConsumer = useContext(HashlistContext);
+    const selectedHashlistFile = useAppSelector(selectHashlistFile);
     const verifyingHashlist = useAppSelector(selectVerifyingHashlist);
     return hashlistConsumer !== null ? (
         <>
-            <textarea id="hashlist" disabled={verifyingHashlist} name="hashlist" onChange={(e) => hashlistConsumer.setHashlist(e.target.value)} value={hashlistConsumer.hashlist} className="min-h-[160px] w-full resize-none rounded border border-gray-300 bg-gray-100 bg-opacity-50 px-3 py-1 text-base leading-6 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"></textarea>
+            <textarea id="hashlist" disabled={verifyingHashlist || selectedHashlistFile !== undefined} name="hashlist" onChange={(e) => hashlistConsumer.setHashlist(e.target.value)} value={hashlistConsumer.hashlist} className="min-h-[160px] w-full resize-none rounded border border-gray-300 bg-gray-100 bg-opacity-50 px-3 py-1 text-base leading-6 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"></textarea>
             {wireless ? (
                 <label htmlFor="hashlist" className="mx-auto mb-5 text-sm leading-7 text-gray-600">
                     {isPMKID ? 'Paste your WPA PMKID here, multiple APs not allowed' : 'Paste your WPA hash (-m 22000) here, only one hash per task allowed'}
                     <span className="text-red-500">*</span>
                 </label>
             ) : (
-                <label htmlFor="hashlist" className="mx-auto text-sm leading-7 text-gray-600">
+                <label htmlFor="hashlist" className="mx-auto text-fl-sm leading-7 text-gray-600">
                     Paste your hashlist here
                     <span className="text-red-500">*</span>
                 </label>
