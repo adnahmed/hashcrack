d757de9bf7067000b3c25cefc43582cff3e93d3d
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-07-18 09:38:39 +0500

    use effect of verifying in second stage of wizard

--- features/VerifyHashlist/VerifyHashlist.tsx
+++ features/VerifyHashlist/VerifyHashlist.tsx
@@ -1,10 +1,17 @@
+import Loading from '@/components/ui/Loading';
 import { useAppSelector } from '@/lib/redux/store';
-import { selectHashlistVerified, selectParsedHashlist, selectRejectedHashlist } from '../NewTask/newTaskSlice';
+import { selectHashlistVerified, selectParsedHashlist, selectRejectedHashlist, selectVerifyingHashlist } from '../NewTask/newTaskSlice';
 
 function VerifyHashlist() {
+    const verifyingHashlist = useAppSelector(selectVerifyingHashlist);
     const parsedHashes = useAppSelector(selectParsedHashlist);
     const rejectedHashes = useAppSelector(selectRejectedHashlist);
     const hashlistVerified = useAppSelector(selectHashlistVerified);
+    if (verifyingHashlist)
+        return <>
+            <Loading />
+            <p className={'text-fl-md'}>Verifying Hashes...</p>
+        </>
     return (
         <div>
             Parsed: <br /> {parsedHashes}
