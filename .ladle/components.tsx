0a27443d958c396ab28f65b1a30a02976d12fb48
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-07-25 17:07:35 +0500

    fix ladle store error

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -1,17 +1,13 @@
-import type { GlobalProvider } from "@ladle/react";
-import "inter-ui/inter.css";
-import { ThemeProvider } from "next-themes";
-import React from "react";
-import { Provider as ReduxProvider } from "react-redux";
-import store from "../lib/redux/store";
-import "./globals.scss";
+import type { GlobalProvider } from '@ladle/react';
+import 'inter-ui/inter.css';
+import { ThemeProvider } from 'next-themes';
+import React from 'react';
+import { Provider as ReduxProvider } from 'react-redux';
+import { makeStore } from '../lib/redux/store';
+import './globals.scss';
 
-export const Provider: GlobalProvider = ({
-  children,
-  globalState,
-  storyMeta,
-}) => (
-  <ReduxProvider store={store}>
-    <ThemeProvider>{children}</ThemeProvider>
-  </ReduxProvider>
+export const Provider: GlobalProvider = ({ children, globalState, storyMeta }) => (
+    <ReduxProvider store={makeStore()}>
+        <ThemeProvider>{children}</ThemeProvider>
+    </ReduxProvider>
 );
