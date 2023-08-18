2c6f4b8e84bbfb2a6023163bd2d39c357c3790b7
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-08-18 15:04:57 +0500

    add withSessionRoute and withSessionSSr

--- lib/session.ts
+++ lib/session.ts
@@ -1,6 +1,8 @@
 // this file is a wrapper with defaults to be used in both API routes and `getServerSideProps` functions
 import type { TurnstileServerValidationResponse } from "@marsidev/react-turnstile";
 import type { IronSessionOptions } from "iron-session";
+import { withIronSessionApiRoute, withIronSessionSsr } from 'iron-session/next';
+import { GetServerSidePropsContext, GetServerSidePropsResult, NextApiHandler } from "next";
 export const cookieName = "cloudflare_session"
 export const sessionOptions: IronSessionOptions = {
   password: process.env.SECRET_COOKIE_PASSWORD,
@@ -9,6 +11,21 @@ export const sessionOptions: IronSessionOptions = {
     secure: process.env.NODE_ENV === "production",
   },
 };
+
+export function withSessionRoute(handler: NextApiHandler) {
+  return withIronSessionApiRoute(handler, sessionOptions);
+}
+
+// Theses types are compatible with InferGetStaticPropsType https://nextjs.org/docs/basic-features/data-fetching#typescript-use-getstaticprops
+export function withSessionSsr<
+  P extends { [key: string]: unknown } = { [key: string]: unknown },
+>(
+  handler: (
+    context: GetServerSidePropsContext,
+  ) => GetServerSidePropsResult<P> | Promise<GetServerSidePropsResult<P>>,
+) {
+  return withIronSessionSsr(handler, sessionOptions);
+}
 
 // This is where we specify the typings of req.session.*
 declare module "iron-session" {
