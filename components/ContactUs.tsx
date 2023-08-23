95f706dc55816aef88159bb957f56dc925de5e53
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-08-23 11:39:54 +0500

    give transition to submit button

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -45,14 +45,14 @@ export default function ContactUs() {
                                 <label htmlFor="message" className="text-sm leading-7 text-gray-600">
                                     Message<span className="text-red-500">*</span>
                                 </label>
-                                <textarea id="message" name="message" className="h-32 w-full resize-none rounded border border-gray-300 bg-gray-100 bg-opacity-50 px-3 py-1 text-base leading-6 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"></textarea>
+                                <textarea id="message" name="message" className="h-32 w-full resize-none rounded border border-gray-300 bg-gray-100 bg-opacity-50 px-3 py-1 text-base leading-6 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200" />
                             </div>
                         </div>
                         <span className="py-2 text-xs leading-4 text-gray-500">
                             Fields marked with <span className="text-red-500">*</span> are required.
                         </span>
                         <div className="w-full p-2">
-                            <button className="mx-auto flex rounded border-0 bg-[var(--theme)] px-8 py-2 text-lg text-white hover:bg-[var(--theme-bg)] hover:text-[var(--theme)] focus:outline-none">Submit</button>
+                            <button className="mx-auto flex rounded border-0 bg-[var(--theme)] px-8 py-2 text-lg text-white transition duration-300 ease-in-out hover:bg-[var(--theme-bg)] hover:text-[var(--theme)] focus:outline-none">Submit</button>
                         </div>
                     </div>
                 </div>
