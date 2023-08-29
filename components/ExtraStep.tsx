9c215cebcd0bb1807498d82bb00bc0c148055931
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-08-29 23:26:27 +0500

    use patternfly checkbox

--- components/ExtraStep.tsx
+++ components/ExtraStep.tsx
@@ -1,14 +1,21 @@
+import { Checkbox } from '@patternfly/react-core';
+import { useState } from 'react';
 
 function ExtraStep() {
+    const [isPrivacyMode, setIsPrivacyMode] = useState(false);
     return (
         <div>
-            <label>
-            <input type="checkbox" />
-            Privacy Mode ({process.env.NEXT_PUBLIC_Privacy_Mode_Price})
-            </label>
+            <Checkbox
+                id="privacy_mode"
+                label={`Privacy Mode (${process.env.NEXT_PUBLIC_Privacy_Mode_Price})`}
+                isChecked={isPrivacyMode}
+                onChange={() => {
+                    setIsPrivacyMode(!isPrivacyMode);
+                }}
+                name="check4"
+            />
         </div>
     );
 }
-
 
 export default ExtraStep;
