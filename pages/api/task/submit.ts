7298c9af04b9c566a563fa6c409a35fb7e619ca7
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-09-05 11:25:45 +0500

    init submit post api

--- pages/api/task/submit.ts
+++ pages/api/task/submit.ts
@@ -1,46 +1,46 @@
-import applyCors from "@/lib/corsMiddleware";
-import requiresToken from "@/lib/requiresToken";
+import applyCors from '@/lib/corsMiddleware';
+import requiresToken from '@/lib/requiresToken';
+import { getSessionData } from '@/lib/session';
 import Cors from 'cors';
-import type { NextApiRequest, PageConfig } from "next";
-import {
-  HttpCode,
-  Post,
-  Req,
-  createHandler
-} from "next-api-decorators";
+import type { NextApiRequest, NextApiResponse, PageConfig } from 'next';
+import { HttpCode, Post, Req, Res, createHandler } from 'next-api-decorators';
 const cors = Cors({
-  origin: "crackq.me",
-  methods: ["GET", "POST"],
+    origin: 'crackq.me',
+    methods: ['POST'],
 });
 
 export interface SubmittedTaskResponse {
-  task: {
-    id: string
-  }
+    task: {
+        id: string;
+        created_by: string;
+    };
+    success: boolean;
 }
 
 class TaskSubmissionHandler {
-  @Post()
-  @requiresToken()
-  @applyCors(cors)()
-  @HttpCode(200)
-  async submit(
-    @Req() req: NextApiRequest) {
-    return {
-      task: {
-        id: 1,
-      }
-    };
-  }
+    @Post()
+    @requiresToken()
+    @applyCors(cors)()
+    @HttpCode(200)
+    async submit(@Req() req: NextApiRequest, @Res() res: NextApiResponse) {
+        const sessionData = await getSessionData(req, res);
+        // TODO: Create task in db.
+        return {
+            ...sessionData,
+            success: true,
+            task: {
+                id: 1,
+            },
+        };
+    }
 }
 
-
 export const config: PageConfig = {
-  api: {
-    bodyParser: {
-      sizeLimit: "5000mb",
+    api: {
+        bodyParser: {
+            sizeLimit: '500mb',
+        },
     },
-  },
 };
 
 export default createHandler(TaskSubmissionHandler);
