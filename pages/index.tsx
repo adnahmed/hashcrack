b2bf585e8cd4775a3fc271cf9e8c0ac7feda5940
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-09-06 19:49:42 +0500

    use next/dynamic instead of legacy lazy+Suspense

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,12 @@ import { FullLoading } from '@/components/ui/Loading';
 import { selectCaptchaValidated } from '@/features/Captcha/captchaSlice';
 import { selectActiveWizardTab } from '@/features/Navigation/navigationSlice';
 import { useAppSelector } from '@/lib/redux/store';
+import dynamic from 'next/dynamic';
 import Head from 'next/head';
-import { Suspense, lazy } from 'react';
 import { NextPageWithLayout } from './_app';
-const MainPage = lazy(() => import('@/components/MainPage'));
+const DynamicMainPage = dynamic(() => import('@/components/MainPage'), {
+    loading: () => <FullLoading />,
+});
 const Home: NextPageWithLayout = () => {
     const captchaValidated = useAppSelector(selectCaptchaValidated);
     const isWizardTab = useAppSelector(selectActiveWizardTab);
@@ -18,15 +20,7 @@ const Home: NextPageWithLayout = () => {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.svg" />
             </Head>
-            <div className={`${isWizardTab ? 'flex h-screen flex-col' : ''} `}>
-                {!captchaValidated ? (
-                    <IntialCheck />
-                ) : (
-                    <Suspense fallback={<FullLoading />}>
-                        <MainPage />
-                    </Suspense>
-                )}
-            </div>
+            <div className={`${isWizardTab ? 'flex h-screen flex-col' : ''} `}>{!captchaValidated ? <IntialCheck /> : <DynamicMainPage />}</div>
         </>
     );
 };
