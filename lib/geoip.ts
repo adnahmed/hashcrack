640768cbf07e67b31e17d5a5c6b00690e3d969f4
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-09-08 11:13:29 +0500

    change path to geolite db

--- lib/geoip.ts
+++ lib/geoip.ts
@@ -1,6 +1,6 @@
 import { Reader } from '@maxmind/geoip2-node';
 import fs from 'fs';
 import path from 'path';
-const dbBuffer = fs.readFileSync(path.resolve(`data/GeoLite2-Country.mmdb`));
+const dbBuffer = fs.readFileSync(path.resolve(`../assets/GeoLite2-Country.mmdb`));
 const reader = Reader.openBuffer(dbBuffer);
 export default reader;
