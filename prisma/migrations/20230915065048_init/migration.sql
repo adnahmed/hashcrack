9403752b98b8c9addd188061671ab2e608f3deb8
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-09-15 11:51:13 +0500

    make country optional

--- prisma/migrations/20230915065048_init/migration.sql
+++ prisma/migrations/20230915065048_init/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "User" (
     "id" UUID NOT NULL,
     "ip_address" INET NOT NULL,
-    "country" VARCHAR(60) NOT NULL,
+    "country" VARCHAR(60),
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
