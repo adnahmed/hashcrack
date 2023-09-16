09f5e1b5b22601fbd22bf79d3b105c1cf4ea4278
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-09-16 20:15:31 +0500

    customize Toaster styling

--- components/Toaster.tsx
+++ components/Toaster.tsx
@@ -1,9 +1,43 @@
 import toast, { ToastBar, Toaster } from 'react-hot-toast';
 
 const CustomToaster = () => (
-    <Toaster>
+    <Toaster
+        containerStyle={{
+            top: 20,
+            left: 20,
+            bottom: 20,
+            right: 20,
+        }}
+        gutter={24}
+        toastOptions={{
+            className: '',
+            style: {
+                padding: '16px',
+                color: '#713200',
+            },
+            success: {
+                style: {
+                    background: 'var(--theme-bg)',
+                    color: 'var(--theme)',
+                },
+                iconTheme: {
+                    primary: 'var(--theme)',
+                    secondary: 'var(--theme-bg)',
+                },
+            },
+            error: {
+                style: {
+                    background: 'var(--theme)',
+                    color: 'white',
+                },
+            },
+        }}>
         {(t) => (
-            <ToastBar toast={t}>
+            <ToastBar
+                toast={t}
+                style={{
+                    ...t.style,
+                }}>
                 {({ icon, message }) => (
                     <>
                         <div aria-label="Close Notification" style={{ cursor: 'pointer' }} onClick={() => toast.dismiss(t.id)}>
