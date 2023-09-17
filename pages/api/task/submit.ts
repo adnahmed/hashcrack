76c87060c453cdebf57ba2703fa838b92c4da227
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-09-17 13:11:43 +0500

    init submit api with validation

--- pages/api/task/submit.ts
+++ pages/api/task/submit.ts
@@ -1,20 +1,70 @@
+import HashtypePrototypes from '@/assets/hashtype_prototypes.json';
+import { Configuration } from '@/features/NewTask/newTaskSlice';
 import applyCors from '@/lib/corsMiddleware';
 import requiresToken from '@/lib/requiresToken';
 import { getSessionData } from '@/lib/session';
+import { ArrayMinSize, ArrayNotEmpty, ArrayUnique, Equals, IsBoolean, IsDefined, IsEmail, IsEnum, IsIn, IsString, ValidateIf } from 'class-validator';
 import Cors from 'cors';
 import type { NextApiRequest, NextApiResponse, PageConfig } from 'next';
-import { HttpCode, Post, Req, Res, createHandler } from 'next-api-decorators';
+import { Body, HttpCode, Post, Req, Res, ValidationPipe, createHandler } from 'next-api-decorators';
+import { TaskData } from '../../../features/NewTask/newTaskSlice';
 const cors = Cors({
     origin: 'crackq.me',
     methods: ['POST'],
 });
 
-export interface SubmittedTaskResponse {
-    task: {
-        id: string;
-        created_by: string;
-    };
+export interface SubmittedTask {
+    id: string;
+    created_at: Date;
     success: boolean;
+}
+const hashcat_modes = HashtypePrototypes
+    // Filter protos with null hashcat modes.
+    .filter(proto => proto.modes.find(d => d.hashcat !== null))
+    .map(proto => proto.modes.map(d => d.hashcat))
+    .reduce((prev, modes) => prev.concat(modes))
+    .filter(m => m !== null)
+    .map(m => m?.toString());
+
+class TaskValidateData implements TaskData {
+    @IsBoolean()
+    @Equals(true, {
+        message: "You must accept terms and conditions before submitting task.",
+    })
+    acceptedTermsAndConditions!: boolean;
+
+    @IsEnum(Configuration, {
+        message: "Unknown attack configuration provided.",
+    })
+    selectedConfig!: Configuration;
+
+    @ValidateIf(o => !o.privacyMode)
+    @IsEmail({}, {
+        message: `You must provide a valid email address if you've opted out of privacy mode.`
+    })
+    resultEmail!: string;
+
+    @IsString()
+    @IsIn(hashcat_modes, {
+        message: "Unrecognized Hash Mode selected."
+    })
+    selectedHashType!: string;
+
+    @ArrayNotEmpty()
+    @ArrayMinSize(1)
+    @ArrayUnique()
+    hashlist!: string[];
+
+    @ArrayUnique()
+    @ArrayMinSize(0)
+    rejectedHashlist!: string[];
+
+    @IsBoolean()
+    privacyMode!: boolean;
+
+    @ValidateIf(o => o.selectedConfig !== Configuration.BASIC)
+    @IsDefined()
+    configData?: unknown;
 }
 
 class TaskSubmissionHandler {
@@ -22,15 +72,15 @@ class TaskSubmissionHandler {
     @requiresToken()
     @applyCors(cors)()
     @HttpCode(200)
-    async submit(@Req() req: NextApiRequest, @Res() res: NextApiResponse) {
+    async submit(@Req() req: NextApiRequest, @Body(ValidationPipe) body: TaskValidateData, @Res() res: NextApiResponse<SubmittedTask>) {
         const sessionData = await getSessionData(req, res);
+        console.log(hashcat_modes);
         // TODO: Create task in db.
-        res.status(200).send({
+        return res.status(200).send({
             ...sessionData,
             success: true,
-            task: {
-                id: 1,
-            },
+            id: '1',
+            created_at: new Date(Date.now())
         });
     }
 }
