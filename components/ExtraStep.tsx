95c281a9c51348e8b2700affd10a2d44d8fe1dfc
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-09-17 12:28:25 +0500

    disable extrastep changes when submittedTask

--- components/ExtraStep.tsx
+++ components/ExtraStep.tsx
@@ -4,7 +4,13 @@ import styles from '@/styles/ExtraStep.module.css';
 import { Checkbox } from '@patternfly/react-core';
 import { useState } from 'react';
 
-function ExtraStep() {
+interface ExtraStepProps {
+    submittingTask: boolean;
+    submittedTask: boolean;
+}
+
+function ExtraStep(props: ExtraStepProps) {
+    const { submittedTask, submittingTask } = props;
     const isPrivacyMode = useAppSelector(selectPrivacyMode);
     const acceptedTermsAndConditions = useAppSelector(selectTermsAndConditions);
     const resultEmail = useAppSelector(selectResultEmail);
@@ -16,6 +22,7 @@ function ExtraStep() {
             <Checkbox
                 id="privacy_mode"
                 label={`Privacy Mode (${process.env.NEXT_PUBLIC_Privacy_Mode_Price}BTC)`}
+                isDisabled={submittedTask || submittingTask}
                 isChecked={isPrivacyMode}
                 onChange={() => {
                     dispatch(changedPrivacyMode(!isPrivacyMode));
@@ -24,14 +31,15 @@ function ExtraStep() {
             <div className={styles.textInput}>
                 <div className="relative">
                     <label htmlFor="name" className="text-sm leading-9 text-gray-600">
-                        Email (Optional)
+                        Email
                     </label>
-                    <input type="email" value={isPrivacyMode ? resultEmail : email} onChange={(e) => setEmail(e.target.value)} onBlur={() => dispatch(changedResultEmail(email))} id="email" name="email" disabled={isPrivacyMode} className="w-full rounded border border-gray-300 bg-gray-100 bg-opacity-50 px-3 py-1 text-base leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out invalid:border-red-300 invalid:bg-[var(--theme-bg)] invalid:ring-2 invalid:ring-red-200 focus:bg-white focus:ring-2 valid:focus:border-indigo-500 valid:focus:ring-indigo-200 disabled:bg-gray-300" />
+                    <input type="email" disabled={submittedTask || isPrivacyMode} value={submittedTask ? resultEmail : isPrivacyMode ? resultEmail : email} onChange={(e) => setEmail(e.target.value)} onBlur={() => dispatch(changedResultEmail(email))} id="email" name="email" className="w-full rounded border border-gray-300 bg-gray-100 bg-opacity-50 px-3 py-1 text-base leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out invalid:border-red-300 invalid:bg-[var(--theme-bg)] invalid:ring-2 invalid:ring-red-200 focus:bg-white focus:ring-2 valid:focus:border-indigo-500 valid:focus:ring-indigo-200 disabled:bg-gray-300" />
                 </div>
             </div>
             <Checkbox
                 id="terms&service"
                 label={`I agree to CrackQ.Me Terms and Conditions`}
+                isDisabled={submittedTask || submittingTask}
                 isChecked={acceptedTermsAndConditions}
                 onChange={() => {
                     dispatch(changedTermsAndConditions(!acceptedTermsAndConditions));
