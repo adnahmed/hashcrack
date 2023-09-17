2c592fe3de72a7680b108542337430117269b510
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-09-17 12:22:48 +0500

    reformat TitleThemed

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,36 +1,35 @@
-import ThemedStyle from "@/styles/TitleThemed.module.scss";
-import { AnimeAnimParams } from "animejs";
-import { HTMLProps } from "react";
-import ReactAnime from "./Anime";
+import ThemedStyle from '@/styles/TitleThemed.module.scss';
+import { AnimeAnimParams } from 'animejs';
+import { HTMLProps } from 'react';
+import ReactAnime from './Anime';
 
 interface TitleProps {
-  text: string;
+    text: string;
 }
 
-export default function Title(props: TitleProps & HTMLProps<"div">) {
-  const { text } = props;
-  /* TODO: Use Splitting: https://splitting.js.org and Anime.js for Effect */
-  const content = text;
-  const animation: AnimeAnimParams = {
-    targets: `#title`,
-    translateY: ["0.75em", 0],
-    translateZ: 0,
-    duration: 750,
-    // delay: (el, i) => 10 * i,
-  };
-  return (
-    <div>
-      <ReactAnime.Anime
-        animeConfig={{
-          easing: "easeInSine",
-        }}
-        _onClick={[animation]}
-        initial={[animation]}
-      >
-        <div id="title" className={`text-fl-lg md:text-fl-xl sm:text-fl-lg lg:text-fl-2xl xl:text-fl-3xl text-center ${ThemedStyle.title} `}>
-          {content}
+export default function Title(props: TitleProps & HTMLProps<'div'>) {
+    const { text } = props;
+    /* TODO: Use Splitting: https://splitting.js.org and Anime.js for Effect */
+    const content = text;
+    const animation: AnimeAnimParams = {
+        targets: `#title`,
+        translateY: ['0.75em', 0],
+        translateZ: 0,
+        duration: 750,
+        // delay: (el, i) => 10 * i,
+    };
+    return (
+        <div>
+            <ReactAnime.Anime
+                animeConfig={{
+                    easing: 'easeInSine',
+                }}
+                _onClick={[animation]}
+                initial={[animation]}>
+                <div id="title" className={`text-center text-fl-lg sm:text-fl-lg md:text-fl-xl lg:text-fl-2xl xl:text-fl-3xl ${ThemedStyle.title}`}>
+                    {content}
+                </div>
+            </ReactAnime.Anime>
         </div>
-      </ReactAnime.Anime>
-    </div>
-  );
+    );
 }
