98be10d2659eaf68123a9bc9147137f020913c7a
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-09-25 14:04:05 +0500

    manually handle server shutdown

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    // TODO: alert someone on server shutdown.
+    if (process.env.NEXT_MANUAL_SIG_HANDLE) {
+        // this should be added in your custom _document
+        process.on('SIGTERM', () => {
+            console.log('Received SIGTERM: ', 'cleaning up');
+            process.exit(0);
+        });
+
+        process.on('SIGINT', () => {
+            console.log('Received SIGINT: ', 'cleaning up');
+            process.exit(0);
+        });
+    }
+    return (
+        <Html lang="en">
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
